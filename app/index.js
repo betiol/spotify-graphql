4eab79468a7c98e80402d34bdfa287d139ddc386
Nikollas Betiol <nikollasbetiol@gmail.com>
2018-04-20 08:12:22 -0400

    add port

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 // @flow
 'use strict';
-import "babel-polyfill";
+import 'babel-polyfill';
 import app from './app';
 
-const GRAPHQL_PORT = 5000;
+const GRAPHQL_PORT = process.env.PORT || 5000;
 (async () => {
-    await app.listen(GRAPHQL_PORT);
-    console.log(`GraphQL running on localhost:${GRAPHQL_PORT}/graphql`);
+	await app.listen(GRAPHQL_PORT);
+	console.log(`GraphQL running on localhost:${GRAPHQL_PORT}/graphql`);
 })();
